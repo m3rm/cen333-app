10c54c5dc7aee373ff6a27aa03968671d0a518c0
mohamed almualla <mar@mohameds-MacBook-Pro-5.local>
2026-06-17 14:20:51 +0400

    Track environments file so Vercel can build

---
+++ src/environments/environments.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAKjYsUe48lkwst8evsxB4_bYQqe9ga5N0",
+    authDomain: "vitaltrend-app-c3e61.firebaseapp.com",
+    databaseURL: "https://vitaltrend-app-c3e61-default-rtdb.firebaseio.com",
+    projectId: "vitaltrend-app-c3e61",
+    storageBucket: "vitaltrend-app-c3e61.firebasestorage.app",
+    messagingSenderId: "674967755426",
+    appId: "1:674967755426:web:457d19ebe018d6957d55fb"
+  },
+  geminiApiKey: "AQ.Ab8RN6KZRfP9IPzZ4knrrp_YiYnhAtF566i5B5xR8PBBRsOc1g",
+  googleMapsApiKey: "AIzaSyCrhhQu4UFynxnDXIkSlI0hW5pwpCsuxpk",
+  warehouse: {
+    lat: 24.4539,
+    lng: 54.3773,
+    name: "Main Warehouse"
+  }
+};
